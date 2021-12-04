77f69f122f2845fd21a5fb282d2682762e38d6fd
kalashkovpaul <kalashkovpaul@mail.ru>
2021-12-04 11:12:00 +0300

    HW2.1: 1st try

---
+++ task_01/task_01_01.cpp
@@ -0,0 +1,193 @@
+/*
+Реализуйте структуру данных типа “множество строк” на основе динамической хеш-таблицы с открытой 
+адресацией. Хранимые строки непустые и состоят из строчных латинских букв.
+Хеш-функция строки должна быть реализована с помощью вычисления значения многочлена методом 
+Горнера.
+Начальный размер таблицы должен быть равным 8-ми. Перехеширование выполняйте при добавлении 
+элементов в случае, когда коэффициент заполнения таблицы достигает 3/4.
+Структура данных должна поддерживать операции добавления строки в множество, удаления строки 
+из множества и проверки принадлежности данной строки множеству.
+
+1_1. Для разрешения коллизий используйте квадратичное пробирование. i-ая проба
+g(k, i)=g(k, i-1) + i (mod m). m - степень двойки.
+*/
+
+#include <iostream>
+#include <vector>
+
+size_t strHash(const std::string &data) {
+    size_t hash = 0;
+    size_t simple = 19;
+    for (char i : data)
+        hash = hash * simple + i;
+    return hash;
+}
+
+template <class T>
+struct Hasher {
+    size_t operator() (const T& key) const {
+        return key;
+    }
+};
+
+template <>
+struct Hasher<std::string> {
+    size_t operator() (const std::string& key) const {
+        return strHash(key);
+    }
+};
+
+struct ExampleStruct {
+    std::string str;
+};
+
+template <>
+struct Hasher<ExampleStruct> {
+    size_t operator() (const ExampleStruct& key) const {
+        return strHash(key.str);
+    }
+};
+
+template <class T>
+struct DefaultEqual {
+    bool operator() (const T& l, const T& r) const {
+        return l == r;
+    }
+};
+
+template <class Key, class Equal=DefaultEqual<Key>, class Hash=Hasher<Key>>
+class HashSet  {
+public:
+    HashSet() {
+        HashElement newElement = HashElement();
+        std::vector<HashElement> tmp(initialSize, newElement);
+        buckets = tmp;
+        hash = Hash();
+        equal = Equal();
+        itemsCount = 0;
+    }
+
+    HashSet(const HashSet &) = delete;
+    HashSet& operator= (const HashSet&) = delete;
+
+    ~HashSet() = default;
+
+    bool add(const Key& key)
+    {
+        if (has(key)) {
+            return false;
+        }
+        if (itemsCount * 100 > buckets.size() * maxLoadFactor)
+            grow();
+        int i = 0;
+        size_t bucketIdx = 0;
+        do {
+            bucketIdx = squareProbeHash(key, i++) % buckets.size();
+        } while (buckets[bucketIdx].condition == busy && i < buckets.size());
+        buckets[bucketIdx].key = key;
+        buckets[bucketIdx].condition = busy;
+        itemsCount++;
+        return true;
+    }
+
+    bool has(const Key& key) {
+        size_t i = 0;
+        size_t bucketIdx = squareProbeHash(key, i++) % buckets.size();
+        do {
+            if (buckets[bucketIdx].condition == busy && equal(buckets[bucketIdx].key, key))
+                return true;
+            else if (buckets[bucketIdx].condition == free)
+                return false;
+            else {
+                bucketIdx = squareProbeHash(key, i++) % buckets.size();
+            }
+        }
+        while (i < buckets.size());
+        return false;
+    }
+
+    bool del(const Key& key) {
+        size_t i = 0;
+        size_t bucketIdx = squareProbeHash(key, i) % buckets.size();
+        while (i++ < buckets.size()) {
+            if (buckets[bucketIdx].condition == busy && equal(buckets[bucketIdx].key, key)) {
+                buckets[bucketIdx].condition = deleted;
+                itemsCount--;
+                return true;
+            }
+            else {
+                bucketIdx = squareProbeHash(key, i) % buckets.size();
+            }
+        }
+        return false;
+    }
+
+private:
+    static constexpr int initialSize = 8;
+    static constexpr int free = 1;
+    static constexpr int deleted = 2;
+    static constexpr int busy = 3;
+    static constexpr int maxLoadFactor = 75;
+    static constexpr int sizeIteration = 2;
+
+    struct HashElement {
+        Key key;
+        int condition;
+
+        HashElement(): key(), condition(free) { }
+        HashElement(Key& key): key(key), condition(free) { }
+    };
+
+    std::vector<HashElement> buckets;
+    Hash hash;
+    Equal equal;
+    size_t itemsCount;
+
+    size_t squareProbeHash(const Key& key, size_t i) {
+        return (hash(key) + i * (i + 1) / 2);
+    }
+
+    void grow() {
+        std::vector<HashElement> newBuckets(buckets.size() * sizeIteration);
+        size_t newIndex = 0;
+        size_t oldIndex = 0;
+        for (size_t i = 0; i < buckets.size(); i++) {
+            oldIndex = squareProbeHash(buckets[i].key, 0) % buckets.size();
+            newIndex = squareProbeHash(buckets[i].key, 0)% (buckets.size() * sizeIteration); 
+            if (buckets[oldIndex].condition == busy) {
+                newBuckets[newIndex] = buckets[oldIndex];
+                buckets[oldIndex].condition = deleted;
+            }
+        }
+        buckets = std::move(newBuckets);
+    }
+};
+
+int main() {
+    HashSet<std::string> set;
+    std::string key;
+    char operation = 0;
+    while (std::cin >> operation >> key) {
+        bool result = false;
+        switch (operation) {
+            case '+':
+                result = set.add(key);
+                break;
+            case '?':
+                result = set.has(key);
+                break;
+            case '-':
+                result = set.del(key);
+                break;
+            default:
+                break;
+        }
+        if (result) {
+            std::cout << "OK" << std::endl;
+        } else {
+            std::cout << "FAIL" << std::endl;
+        }
+    }
+    return 0;
+}
+
